808b719199c62e6065ba41d832bf78ec275c0504
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-15 07:49:39 -0500

    complete initial devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Firefly::Application.routes.draw do
+
+  root 'static_pages#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
