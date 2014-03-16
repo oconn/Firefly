1caaac70e75fb52220a65fcb0154d6598caf0ea5
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-16 17:37:57 -0500

    add s3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,14 @@ Firefly::Application.configure do
   config.assets.debug = true
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_protocol => "https",
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Firefly::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_protocol => "https",
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
