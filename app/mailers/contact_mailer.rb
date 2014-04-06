14b37ae5af52281cde7b024598880d0ac76cfb6a
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-04-06 12:59:20 -0400

    precompile assets

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,7 @@
+class ContactMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  def send_form
+
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,18 @@ Firefly::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            '<username>',
+    password:             '<password>',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
+  
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_protocol => "https",
