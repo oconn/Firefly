044572ebfd7538d237d6e9ba5fd6ebb1f60678a1
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-04-24 14:33:29 -0400

    remove turbo links and add quotes to homepage

---
+++ app/assets/images/Angular Application_files/app.js
@@ -0,0 +1,5 @@
+var myApp = angular.module("myApp", ["ngRoute"]);
+
+myApp.config(function($routeProvider){
+  console.log($routeProvider)  
+})
---
+++ app/assets/images/Angular Application_files/controllers.js
@@ -0,0 +1,9 @@
+myApp.controller("indexController", function($scope){
+  $scope.staff = [
+    {name: "Harvey Specter"},
+    {name: "Mike Ross"},
+    {name: "Rachel Zane"},
+    {name: "Donna Paulsen"},
+    {name: "Jessica Pearson"}
+  ]
+});
